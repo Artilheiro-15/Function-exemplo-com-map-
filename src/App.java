90d2646aa65d8941efdc858f13c209f12646b0a7
Atila jr <atilla.junior33@gmail.com>
2023-01-10 15:04:37 -0300

    Expressao lambda inline

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
 import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import util.UpperCaseName;
 
 public class App {
 
@@ -16,9 +14,10 @@ public class App {
     list.add(new Product("Tablet", 350.50));
     list.add(new Product("HD Case", 80.90));
 
-    Function<Product, String> func = p -> p.getName().toUpperCase();
-
-    List<String> names = list.stream().map(func).collect(Collectors.toList());
+    List<String> names = list
+      .stream()
+      .map(p -> p.getName().toUpperCase())
+      .collect(Collectors.toList());
 
     names.forEach(System.out::println);
 
